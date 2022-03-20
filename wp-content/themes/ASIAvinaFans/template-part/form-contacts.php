0bc2997f7aa2e854de6e395d246da2f04e97af75
huynhsang2203 <huynhsang220300@gmail.com>
2022-03-20 15:16:06 +0700

    a

--- wp-content/themes/ASIAvinaFans/template-part/form-contacts.php
+++ wp-content/themes/ASIAvinaFans/template-part/form-contacts.php
@@ -1,46 +1,21 @@
 <div class="form-box">
-    <div>
-        <h2>PRODUCT REGISTRATION<h2>
-        </div>
-        <div>
-            <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('form')):?><?php endif;
-            ?>
-        </div>
+<div>
+<h2>PRODUCT REGISTRATION<h2>
+</div>
+<div>
+<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('form')):?><?php endif;
+?>
+</div>
 
-        <div class="container">
-            <div class="row align-items-start">
-                <div class="col">
-                    One of three columns
-                </div>
-                <div class="col">
-                    One of three columns
-                </div>
-                <div class="col">
-                    One of three columns
-                </div>
-            </div>
-            <div class="row align-items-center">
-                <div class="col">
-                    One of three columns
-                </div>
-                <div class="col">
-                    One of three columns
-                </div>
-                <div class="col">
-                    One of three columns
-                </div>
-            </div>
-            <div class="row align-items-end">
-                <div class="col">
-                    One of three columns
-                </div>
-                <div class="col">
-                    One of three columns
-                </div>
-                <div class="col">
-                    One of three columns
-                </div>
-            </div>
-        </div>
+<div class="container">
+<div class="row"style="border: 1px solid red">
+<div class="col-xs-2"id="p1">One</div>
+<div class="col-xs-2 col-half-offset"id="p2">Two</div>
+<div class="col-xs-2 col-half-offset"id="p3">Three</div>
+<div class="col-xs-2 col-half-offset"id="p4">Four</div>
+<div class="col-xs-2 col-half-offset"id="p5">Five</div>
+<div>lorem</div>
+</div>
+</div>
 
-    </div>
+</div>
